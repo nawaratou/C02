fd0f724bca0c9bf1e69ae37ca117d3cdde64995f
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-15 23:33:50 +0100

    mark

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	int n;
-
-	printf("$ ./even_odd\n");
-	scanf("%d", &n);
-	if(n % 2 == 1) 
-	{
-		printf("%d le nombre est impair\n",n);
-	}
-	else
-	{
-		printf("%d le nombre est pair\n",n); 
-	}
-	return 0;
+    int n;
+    scanf("%d", &n);
+    if (n % 2 == 0) {
+        printf("%d est un nombre pair.\n", n);
+    } else {
+        printf("%d est un nombre impair.\n", n);
+    }
+    return 0;
 }
-
