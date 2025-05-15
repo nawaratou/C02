3ca233a0ccc99011b2b0efc5b30e2f20df1ec3d9
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-15 23:29:21 +0100

    sert

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-// Programme qui échange deux nombres sans utiliser de variable temporaire
 int main() {
     int a, b;
-
-    printf("Entrez deux nombres : ");
     scanf("%d %d", &a, &b);
-
+    
     // Échange sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
-
-    printf("Après échange: a = %d, b = %d\n", a, b);
+    
+    printf("Après échange: a = %d, b = %d\n", a, b); // Format exact
     return 0;
 }
