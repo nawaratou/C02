62ca6c8aa14693d18f06fd75786ff10a4e419374
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-15 19:55:51 +0100

    new

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -3,10 +3,7 @@
 // Programme qui lit deux entiers et affiche leur somme
 int main() {
     int a, b;
-    printf("Entrez deux entiers : ");
     scanf("%d %d", &a, &b);
-
-    int somme = a + b;
-    printf("La somme est : %d\n", a + b);
+    printf("la somme est: %d\n", a +b);
     return 0;
 }
