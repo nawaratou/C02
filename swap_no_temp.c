846e8632b93ccf3f2279059e8c4d1911c9ca8b7c
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-15 23:47:57 +0100

    vrai

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,13 +2,13 @@
 
 int main() {
     int a, b;
-    scanf("%d %d", &a, &b);
-    
+    scanf("%d %d", &a, &b);  // Pas de printf avant
+
     // Échange sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
-    
-    printf("Après échange: a = %d, b = %d\n", a, b); // Format exact
+
+    printf("Après échange: a = %d, b = %d\n", a, b);  // Texte exact
     return 0;
 }
