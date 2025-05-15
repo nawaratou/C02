c7e71ea1841061dddf18241efceb5be6cf1b368d
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-15 23:18:39 +0100

    jeu

--- factorial.c
+++ factorial.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+
 int main() {
-	int N;
-	int f = 1;
-	printf("$ ./factorial\n");
-	scanf("%d", &N);
-	for ( int i = 1; i <= N;i++)
-	{
-		f*=i;
-	}
-	printf("le factoriel de %d est : %d\n" ,N ,f);
-	return 0;
+    int N;
+    scanf("%d", &N);
+
+    if (N < 0) {
+        printf("Le factoriel n'est pas défini pour les nombres négatifs.\n");
+        return 1;
+    }
+
+    long factoriel = 1;
+    for (int i = 1; i <= N; i++) {
+        factoriel *= i;
+    }
+
+    printf("Le factoriel de %d est : %ld\n", N, factoriel);
+    return 0;
 }
