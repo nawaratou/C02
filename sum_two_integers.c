af8f46b9bfcfd73b4b7debb66353390da0a080e2
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-15 23:24:13 +0100

    sonne

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-// Programme qui lit deux entiers et affiche leur somme
 int main() {
     int a, b;
-    scanf("%d %d", &a, &b);
-    printf("la somme est: %d\n", a +b);
+    scanf("%d %d", &a, &b); // Pas de printf avant le scanf
+    printf("La somme est : %d\n", a + b); // Format exact demandé
     return 0;
 }
