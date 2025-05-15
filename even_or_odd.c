eb53ea2337882dafb262b27ab6f8adbda0ab3af4
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-15 18:43:35 +0100

    corrige

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-
-// Programme qui vérifie si un nombre est pair ou impair
-int main() {
-    int n;
-    printf("Entrez un entier : ");
-    scanf("%d", &n);
-
-    if (n % 2 == 0) {
-        printf("%d est un nombre pair.\n", n);
-    } else {
-        printf("%d est un nombre impair.\n", n);
-    }
-    return 0;
-}
+#include<stdio.h>
+int main()
+	{int n;
+	printf("$ ./even_odd\n");
+	scanf("%d",&n);
+	if(n%2==0)
+		{printf("%d est un nombre pair",n);
+		}
+	else
+		{printf("%d est un nombre impair",n);
+		}
+	return 0;		
+	}
